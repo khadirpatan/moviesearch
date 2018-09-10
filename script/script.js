f3b68155fc15b9db0be08d9c258128d0b3a8bbec
khadirpatan <khadirpatan1@gmail.com>
2018-09-10 11:04:47 +0530

    second commit

--- script/script.js
+++ script/script.js
@@ -8,51 +8,65 @@ $("#idLink").click(function(){
 
 
 	$("#inputYear").hide();
+	$("#inputName").val("");
+	$("#inputYear").val("");
 
- $("#inputName").attr('placeholder', 'Enter ImdbId');
+ 	$("#inputName").attr('placeholder', 'ImdbId..');
 
-});
+	});
 
 $("#titleLink").click(function(){
 
 
-if($('#inputYear').is(':hidden')) {
-    $("#inputYear").show();
+	if($('#inputYear').is(':hidden')) {
+    	$("#inputYear").show();
+    	$("#inputName").val("");
 
-}
- $("#inputName").attr('placeholder', 'Enter MovieName');
+			}
+ 	$("#inputName").attr('placeholder', 'Title..');
 
-});
-
-
-
-
-
-
-
-
-
-	$("#imbdReset").click(function(){
-
-		$("#imbd").val("");
 	});
 
 
 
 
-	$("#imbdSearch").click(function(){
+$("#search").click(function(){
 
-		let value= $("#imbd").val();
-		alert(value);
+var k = $("#inputName").attr('placeholder');
+alert(k);
 
-		if (value==""){
-			alert("Enter a movie Id");
+if( k == 'Title..'){
+
+			alert("in title");
+
 		}
 
 		else{
-
-			alert("you have entered an ");
+			alert("in year");
 		}
+
+
+	$.ajax({
+
+		type : 'GET',
+		datatype : 'json',
+		async:true,
+
+		
+
+		url : ' http://www.omdbapi.com/?i=tt3896198&apikey=133949cb&s='+ $("#inputName").val() || ' http://www.omdbapi.com/?i=tt3896198&apikey=133949cb&i='+ $("#inputName").val() ,
+
+		success : (respone) =>{
+			console.log(respone);
+		}
+
+
+
+
+	});
+
+
+
 	});
 
 
